3821229e80853561f5713cec24b3baa7f89d5e3e
Ben Jones <benpaddlejones@outlook.com>
2015-06-13 20:45:33 +1000

    add new google classroom page

--- resourcesmenu.js
+++ resourcesmenu.js
@@ -4,4 +4,5 @@ document.write("<li><a href=\"accpdf.html\">Accessible PDF</a></li>");
 document.write("<li><a href=\"byod_info.html\">BYOD Information</a></li>");
 document.write("<li><a href=\"CreativeCommons.html\">Creative Commons</a></li>");
 document.write("<li><a href=\"designprocess.html\">Design Process</a></li>");
+document.write("<li><a href=\"gcroom.html\">Google Classroom</a></li>");
 document.write("<li><a href=\"techdrawing.html\">Technical Drawing</a></li>");
